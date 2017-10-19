42daa797432dc571f882386c1b812a13607df6b8
Rene Barto <rene.barto66@gmail.com>
2017-10-19 17:34:08 +0200

    Added binutils-2.26.1

---
+++ tools/cmake/toolchainfile.cmake
@@ -0,0 +1,55 @@
+#
+# Automatically generated file; DO NOT EDIT.
+# CMake toolchain file
+#
+
+set(STAGING_DIR $ENV{STAGING_DIR})
+
+# Point cmake to the location where we have our custom modules,
+# so that it can find our custom platform description.
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# Set the {C,CXX}FLAGS appended by CMake depending on the build type.
+# CMake defaults these variables with -g and/or
+# -O options, and they are appended at the end of the argument list,
+# so the Buildroot options are overridden. Therefore these variables
+# have to be cleared, so that the options passed in CMAKE_C_FLAGS do
+# apply.
+#
+# Note:
+#   Do not override any of these flag variables.
+set(CMAKE_C_FLAGS_DEBUG "" CACHE STRING "Debug CFLAGS")
+set(CMAKE_CXX_FLAGS_DEBUG "" CACHE STRING "Debug CXXFLAGS")
+set(CMAKE_C_FLAGS_RELEASE " -DNDEBUG" CACHE STRING "Release CFLAGS")
+set(CMAKE_CXX_FLAGS_RELEASE " -DNDEBUG" CACHE STRING "Release CXXFLAGS")
+
+# Build type configuration
+set(CMAKE_BUILD_TYPE Release CACHE STRING "Build configuration")
+
+# Defaults flags.
+set(CMAKE_C_FLAGS "-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -O2" CACHE STRING "Build CFLAGS")
+set(CMAKE_CXX_FLAGS "-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -O2" CACHE STRING "Build  CXXFLAGS")
+set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "Build  LDFLAGS for executables")
+
+set(CMAKE_INSTALL_SO_NO_EXE 0)
+
+set(CMAKE_PROGRAM_PATH "${STAGING_DIR}/usr/bin")
+set(CMAKE_SYSROOT "/")
+set(CMAKE_FIND_ROOT_PATH "${STAGING_DIR}")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(ENV{PKG_CONFIG_SYSROOT_DIR} "")
+
+set(CMAKE_C_COMPILER "/usr/bin/gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/g++")
+if(0)
+  set(CMAKE_Fortran_FLAGS_DEBUG "" CACHE STRING "Debug Fortran FLAGS")
+  set(CMAKE_Fortran_FLAGS_RELEASE " -DNDEBUG" CACHE STRING "Release Fortran FLAGS")
+  set(CMAKE_Fortran_FLAGS "-O2" CACHE STRING "Buildroot FCFLAGS")
+  set(CMAKE_Fortran_COMPILER "/usr/bin/gfortran")
+endif()
